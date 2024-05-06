a7c98d02056e90ebc38b53c839c0dc4883818fdb
alfikihnbs <alfikihnurbanyuseg@gmail.com>
2024-05-05 23:43:24 -0700

    latihanbutton1

---
+++ button_alfikih/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glow Shadow Hover Button</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <a href="#" class="btn">Button</a><br>
+    <a href="#" class="btn2">Button</a><br>
+    <a href="#" class="btn3">Button</a>
+    
+
+</body>
+</html>
